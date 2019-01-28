6fee2f698d7ebbe1a8b19ff4d26b16e934fbd059
Huan Ming Liao <hmliao14@gmail.com>
2019-01-28 14:29:49 -0800

    imported browser router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <BrowserRouter>
+        <div className="App">
+        </div>
+      </BrowserRouter>
     );
   }
 }
