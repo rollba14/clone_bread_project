270806efdeb02dbc821bd2ca764c20a852e589a4
Huan Ming Liao <hmliao14@gmail.com>
2019-01-28 14:42:26 -0800

    updated home component and added routes to app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import Main from './components/main'
+import About from './components/about'
+import Home from './components/home'
+import Help from './components/help'
+import News from './components/news'
+import Application from './components/application'
+import Donation from './components/donation'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
+          <Switch>
+            <Route exact path='/' component={Main}/>
+            <Route path='/about' component={About}/>
+            <Route path='/home' component={Home}/>
+            <Route path='/help' component={Help}/>
+            <Route path='/news' component={News}/>
+            <Route path='/applications' component={Application}/>
+            <Route path='/donation' component={Donation}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+    <div className='home'>
+      This is the home page
+    </div>
+  )
+}
+
+export default Home;
