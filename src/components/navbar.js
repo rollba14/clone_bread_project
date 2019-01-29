e3a2e6fb63bd56d40ca0953c9509d9cd66b6f424
Huan Ming Liao <hmliao14@gmail.com>
2019-01-28 17:25:03 -0800

    layout of navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Link, NavLink, withRouter} from 'react-router-dom'
+import React, {Component} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 import {
   Collapse,
   Navbar,
@@ -14,12 +14,55 @@ import {
   DropdownItem } from 'reactstrap';
 
 
-const Navbar = () => {
-  return(
-    <Navbar expand="md">
+class MyNavbar extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isOpen: false
+    }
+  }
+  toggleNavbar=()=>{
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render(){
+    return(
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="" className="mr-auto">
+            <Link to="/">Logo Here</Link>
+          </NavbarBrand>
+          <NavbarToggler className="text-primary" onClick={this.toggleNavbar}>
+            <span className="navbar-toggler-icon"></span>
+          </NavbarToggler>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <Link to="/about" onClick={this.toggleNavbar}>What We Do</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/home" onClick={this.toggleNavbar}>Who We Are</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/help" onClick={this.toggleNavbar}>Get Involved</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/news" onClick={this.toggleNavbar}>News & Events</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/application" onClick={this.toggleNavbar}>Apply to Program</Link>
+              </NavItem>
+              <NavItem>
+                <Link to="/donation" onClick={this.toggleNavbar}>Donate!</Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
 
-    </Navbar>
-  )
+        </Navbar>
+      </div>
+    )
+  }
 }
 
-export default Navbar;
+export default withRouter(MyNavbar);
