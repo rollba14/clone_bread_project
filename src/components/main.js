f5e2829c9057b0ee7bfd7af67d084c57a4338531
Huan Ming Liao <hmliao14@gmail.com>
2019-02-03 17:21:44 -0800

    added initial layout and styling 1st section of main.js

--- src/components/main.js
+++ src/components/main.js
@@ -9,6 +9,14 @@ const Main = () => {
             <div className="display-3"> Baking Through Barriers</div>
           </div>
         </Jumbotron>
+        <div className="message container px-md-3 text-center">
+          <div className="col-md-10 px-md-4 mx-auto">
+            <h1 className="display-5">Help for Today. Hope for Tomorrow.</h1>
+            <p>Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text </p>
+            <div className="border-top-3 border-primary col-5 mx-auto text-white mb-3"> </div>
+            <p>Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text Placeholder Text </p>
+          </div>
+        </div>
     </div>
   )
 }
