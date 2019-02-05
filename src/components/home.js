432b478948bf2d8b85c8ae16ee4152813020c276
Huan Ming Liao <hmliao14@gmail.com>
2019-02-04 22:56:39 -0800

    added skeleton for 2nd section of home page

--- src/components/home.js
+++ src/components/home.js
@@ -10,6 +10,32 @@ const Home = () => {
           <p className="h3">Opening doors through training</p>
         </div>
       </Jumbotron>
+      <div className="container-fluid p-0 m-0 text-center">
+        <div className="row">
+          <div className="what_we_offer col-12 py-5">
+            <h3 className="display-3">What We Offer</h3>
+            <p className="muted h3">The Bakery Production Bootcamp is a 3-week job training program. It is FREE for low income adults in the San Francisco Bay Area. We employ an intensive training curriculum comprised of two main components – Commercial Baking Training and Workforce Essential Life Skills.</p>
+            <div className="row">
+              <div className="col-md-6 col-12">
+                <h4>Participants Gain</h4>
+              </div>
+              <div className="col-md-6 col-12">
+                <h4>Graduates Receive</h4>
+              </div>
+            </div>
+          </div>
+
+          <div>
+          </div>
+
+          <div>
+          </div>
+
+          <div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
