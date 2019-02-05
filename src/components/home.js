253b7ed481d9f603716c7af478e72d2543977b2d
Huan Ming Liao <hmliao14@gmail.com>
2019-02-04 22:35:14 -0800

    added and styled home page jumbotron

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Jumbotron } from 'reactstrap';
 
 const Home = () => {
   return(
     <div className='home'>
-      This is the home page
+      <Jumbotron className="jumbotron-fluid text-center text-white">
+        <div className="container-fluid">
+          <h1 className="display-4">Bakery Bootcamp</h1>
+          <p className="h3">Opening doors through training</p>
+        </div>
+      </Jumbotron>
     </div>
   )
 }
