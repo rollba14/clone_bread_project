9a936428fe6494b2d4fc9de2765978155adca2f9
Huan Ming Liao <hmliao14@gmail.com>
2019-02-04 23:14:51 -0800

    added line-height to home page 1st section

--- src/components/home.js
+++ src/components/home.js
@@ -15,7 +15,7 @@ const Home = () => {
           <div className="what_we_offer col-12 py-5">
             <div className="col-md-10 px-5 px-md-4 mx-auto">
               <h3 className="h1 mild_blue_text font-weight-bold">What We Offer</h3>
-              <p className="text-muted h4">The Bakery Production Bootcamp is a 3-week job training program. It is FREE for low income adults in the San Francisco Bay Area. We employ an intensive training curriculum comprised of two main components – Commercial Baking Training and Workforce Essential Life Skills.</p>
+              <p className="text-muted h5">The Bakery Production Bootcamp is a 3-week job training program. It is FREE for low income adults in the San Francisco Bay Area. We employ an intensive training curriculum comprised of two main components – Commercial Baking Training and Workforce Essential Life Skills.</p>
               <div className="row col-md-10 mx-auto bg-white">
                 <div className="col-md-6 col-12">
                   <h4 className="mild_blue_text font-weight-bold">Participants Gain</h4>
