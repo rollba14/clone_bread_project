6420fe8e53cffe059214d7d72af185f62b210c25
Huan Ming Liao <hmliao14@gmail.com>
2019-02-05 22:26:21 -0800

    created bootcamp schedule component for home page

---
+++ src/components/bootcamp_schedule_table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bootcamp_Schedule = () => {
+  return(
+    <div className="bootcamp_schedule">
+      schedule here
+    </div>
+  )
+}
+
+export default Bootcamp_Schedule;
