25a536b87213c2454e468f14b83b74bf9150aa69
Huan Ming Liao <hmliao14@gmail.com>
2019-02-26 23:45:42 -0800

    created a UI component staff with temp boilterplate

---
+++ src/components/staff.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Staff = () => {
+  return(
+    <div className="col-12 col-md-4">
+      <img src={Pamela_headshot} alt="Pamela Headshot"/>
+      <p className="mild_blue_text font-weight-bold h4 mt-3">Pamela Calloway, Esq.</p>
+      <h5 className="mild_blue_text h5">Executive Director and Board Member
+      </h5>
+      <p className="text-muted">
+        Pamela brings deep experience in both workforce and regional economic development and a passion for the mission. She has served as Vice President of Programs for RDJ Enterprises, District Director for Congresswoman Barbara Lee (D-CA13), President of the Board of Commissioners for the Port of Oakland, and has advised mayors, Members of Congress, business associations and nonprofit organizations on the strategic use of resources to develop self-sustaining skills for those with barriers to employment. Pamela is a graduate of Fisk University, summa cum laude and Phi Beta Kappa and Georgetown University Law Center where she served as National Chair for the Black Law Students Association. Ms. Calloway is an active member of the State Bar of California. She is a graduate of TBP Bakery BootcampTM, an accomplishment that highlights her desire to understand the organization and those we serve.
+      </p>
+    </div>
+  )
+}
+
+export default Staff;
