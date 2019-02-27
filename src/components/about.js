934604e71fcceca555a6b23a32dee488eee54672
Huan Ming Liao <hmliao14@gmail.com>
2019-02-27 00:41:10 -0800

    dynamically rendered staff to targeted section

--- src/components/about.js
+++ src/components/about.js
@@ -1,25 +1,24 @@
 import React, {Component} from 'react'
+import Staff from './staff'
 import about_banner_img from '../assets/WhoWeAre_Hero-compressor.jpg';
 import Infographic_compressor from "../assets/Infographic-compressor.jpg"
-import Pamela_headshot from "../assets/Headshots_0003_TBP-Pamela_Calloway-3651-RT3-compressor.jpg"
-import Gram_headshot from '../assets/Headshots-Empty.jpg'
-import Lynn_headshot from '../assets/Headshots_0004_TBP-Lynn_Lukow-3640-RT-compressor.jpg'
-import Usha_headshot from '../assets/Headshots_0001_TBP-Usha_Gongal-3673-RT-compressor.jpg'
-import  Trent_headshot from '../assets/Headshots_0002_TBP-Trent_Cooper-3695-RT-compressor.jpg'
-import Veronica_headshot from '../assets/Headshots_0000_TBP-Veronica_Barron-3632-RT-compressor.jpg'
 
 class About extends Component{
   constructor(props){
     super(props);
+    var staff = require('../staff.json');
     this.state = {
-      isOpen: false
+      staff
     }
   }
 
   render(){
+    const staff_elements = this.state.staff.map(s=>{
+      return (<Staff staff={s}/>)
+    })
     return(
       <div className='about'>
-        <h4 class="h1 mild_blue_text text-center p-5 font-weight-bold">We Prepare People for Work & Life</h4>
+        <h4 className="h1 mild_blue_text text-center p-5 font-weight-bold">We Prepare People for Work & Life</h4>
         <img src={about_banner_img} alt="About Banner Image"/>
         <div className="container-fluid p-0 m-0 text-center">
           <div className="row">
@@ -66,18 +65,8 @@ class About extends Component{
                 <h3 className="p-5 mild_blue_text">Our Staff</h3>
 
                 <div className="row">
-                  <div className="col-12 col-md-4">
-                    <img src={Pamela_headshot} alt="Pamela Headshot"/>
-                    <p className="mild_blue_text font-weight-bold h4 mt-3">Pamela Calloway, Esq.</p>
-                    <h5 className="mild_blue_text h5">Executive Director and Board Member
-                    </h5>
-                    <p className="text-muted">
-                      Pamela brings deep experience in both workforce and regional economic development and a passion for the mission. She has served as Vice President of Programs for RDJ Enterprises, District Director for Congresswoman Barbara Lee (D-CA13), President of the Board of Commissioners for the Port of Oakland, and has advised mayors, Members of Congress, business associations and nonprofit organizations on the strategic use of resources to develop self-sustaining skills for those with barriers to employment. Pamela is a graduate of Fisk University, summa cum laude and Phi Beta Kappa and Georgetown University Law Center where she served as National Chair for the Black Law Students Association. Ms. Calloway is an active member of the State Bar of California. She is a graduate of TBP Bakery BootcampTM, an accomplishment that highlights her desire to understand the organization and those we serve.
-                    </p>
-
-                  </div>
+                  {staff_elements}
                 </div>
-
               </div>
             </div>
 
