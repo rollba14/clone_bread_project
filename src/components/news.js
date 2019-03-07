8f417759dc5825139ba1d9bdb0e89f984558b0f4
Huan Ming Liao <hmliao14@gmail.com>
2019-03-06 23:33:47 -0800

    added news & events page

--- src/components/news.js
+++ src/components/news.js
@@ -3,7 +3,36 @@ import React from 'react'
 const News = () => {
   return(
     <div className='news'>
-      This is the news page
+      <div className="container-fluid p-0 m-0 text-center">
+        <div className="row">
+
+          <div className="col-12 pt-4 pb-3">
+            <div className="col-md-10 px-5 px-md-4 mx-auto">
+              <p className="text-left h2 light_darker_text border-bottom border-gray pb-4">
+                News & Events
+              </p>
+              <div className="text-muted h1 border-bottom border-gray">
+                <p>
+                  Join us at Sip, Savor & Support on April 9th!
+                  <div className="py-1"></div>
+                  Click
+                  <a className="light_darker_text" target="_blank" href="https://www.eventbrite.com/e/youre-invited-sip-savor-support-the-bread-project-tickets-57574843908?aff=ebdssbdestsearch"> HERE </a>
+                  for tickets.
+                </p>
+              </div>
+              <img src={require('../assets/2019-Invite-V7a-1.jpg')} alt="News Photo"/>
+              <br/>
+              <br/>
+              <small className="text-muted font-weight-bold">
+                Click
+                <a className="light_darker_text" target="_blank" href="https://www.eventbrite.com/e/youre-invited-sip-savor-support-the-bread-project-tickets-57574843908?aff=ebdssbdestsearch"> HERE </a>
+                TO SIGN UP FOR THE EVENT!
+              </small>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
