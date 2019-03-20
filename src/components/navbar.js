ba5a82f795f037aff7bf684920cf1443ce9c6c1c
Huan Ming Liao <hmliao14@gmail.com>
2019-03-19 20:32:07 -0700

    animated navbar height/size on scroll

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -18,14 +18,35 @@ class MyNavbar extends Component{
   constructor(props){
     super(props);
     this.state = {
-      isOpen: false
+      isOpen: false,
     }
   }
+
   toggleNavbar=()=>{
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
+  componentDidMount(){
+    let htmlDiv = document.getElementsByTagName('html')[0];
+    let navBar = document.getElementsByClassName('navbar')[0];
+    let collapseDiv = navBar.querySelector('.show, .collapse');
+    document.addEventListener('scroll', (e)=>this.reduceNavBarSize(e,htmlDiv,navBar,collapseDiv), false);
+  }
+
+  reduceNavBarSize(e,htmlDiv,navBar,collapseDiv){
+    let scrolledDistance = htmlDiv.getBoundingClientRect().top;
+    if(scrolledDistance < -100) {
+      navBar.setAttribute('style',
+        'height: 60px; line-height: 3.75em; font-size: .75em; padding: .375em 2.25em;');
+      collapseDiv.setAttribute('style', 'line-height: inherit; margin-top: 0px;');
+    } else if (scrolledDistance >= -50){
+      navBar.setAttribute('style','height: 106px; line-height: 5em; padding: 0.5em 3em;');
+      collapseDiv.setAttribute('style','line-height: 1em');
+    }
+  }
+
   render(){
     return(
         <div>
