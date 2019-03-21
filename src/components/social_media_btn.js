f611592a9f49c58c491686fd4bb408bc5da7c68f
Huan Ming Liao <hmliao14@gmail.com>
2019-03-20 17:50:51 -0700

    added facebook btn and styled btns

--- src/components/social_media_btn.js
+++ src/components/social_media_btn.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import twitter_icon_gray from '../assets/twitter_icon_gray.png';
+import facebook_icon_gray from '../assets/facebook_icon_gray.png';
 
 const Social_Media_Btns = () => {
   return(
     <div className='social_media col-12 py-5'>
       <div className="row">
         <div className="col-md-10 px-5 px-md-4 mx-auto text-left">
-          <p className="light_darker_text font-weight-bold text-left"><small>Share this:</small></p>
-          <a href="https://breadproject.org/?share=twitter&nb=1" className="twitter-mention-button align-items-center" data-text="About Us https://breadproject.org/about-us/" data-show-count="false" target="_blank">
+          <span className="light_darker_text font-weight-bold text-left"><small>Share this:</small></span>
+          <a href="https://breadproject.org/?share=twitter&nb=1" target="_blank">
             <button type="button" className="py-0 btn btn-sm btn-outline-secondary">
                 <img src={twitter_icon_gray}/>
-                Twitter
+                <small>Twitter</small>
+            </button>
+          </a>
+          <a href="https://breadproject.org/home/?share=facebook&nb=1" target="_blank">
+            <button type="button" className="py-0 btn btn-sm btn-outline-secondary">
+                <img src={facebook_icon_gray}/>
+                <small>Facebook</small>
             </button>
           </a>
         </div>
