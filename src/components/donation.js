144ec018c1f62c520df77012cd94141edfb15381
Huan Ming Liao <hmliao14@gmail.com>
2019-03-21 21:13:55 -0700

    styled campaign msg box and donation min-height

--- src/components/donation.js
+++ src/components/donation.js
@@ -5,14 +5,16 @@ import facebook_icon_gray from '../assets/facebook_icon_gray.png';
 class Donation extends Component{
   render(){
     return(
-      <div className='donation'>
-        <div className="campaign_title">
-
+      <div className='donation container-fluid'>
+        <div className="campaign_title text-center">
+          <h1>
+            The Bread Project Fall Matching Grant Campaign
+          </h1>
         </div>
         <div className="row">
-          <div className="col-4">
+          <div className="col-6">
             <div className="campaign_msg">
-              <div>
+              <p className="d-flex justify-content-md-around wrap">
                 <a href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdonorbox.org%2Fdonate-to-the-bread-project" target="_blank" rel="noopener noreferrer">
                   <button type="button" className="py-0 btn btn-sm btn-outline-secondary">
                       <img src={facebook_icon_gray} alt="twitter icon"/>
@@ -31,7 +33,7 @@ class Donation extends Component{
                       <small>Twitter</small>
                   </button>
                 </a>
-              </div>
+              </p>
               <p>
                 Support The Bread Project's mission to provide low-income residents of the Bay Area with free training and job readiness skills for employment in the baking and food service industry.
               </p>
@@ -41,7 +43,7 @@ class Donation extends Component{
             </div>
           </div>
 
-          <div className="col-8">
+          <div className="col-6">
             <div className="donation_form">
 
             </div>
