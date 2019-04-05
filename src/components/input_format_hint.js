7d6164b28482b6c1fb1b25281476155d9c679c35
Huan Ming Liao <hmliao14@gmail.com>
2019-04-05 16:22:53 -0700

    updated form hint

--- src/components/input_format_hint.js
+++ src/components/input_format_hint.js
@@ -7,7 +7,7 @@ const InputFormatHint = () => {
         <li>Only number(s) (0-9), a single dot (.), and optional comma(s) (,) are accepted in custom amount.</li>
         <li>Letters a-z, A-Z and other special characters are NOT accepted in custom amount.</li>
         <li>Comma(s) cannot appear after the dot (.).</li>
-        <li>No white spaces are accepted between numbers</li>
+        <li>No white spaces are accepted between numbers.</li>
       </ul>
     </div>
   )
