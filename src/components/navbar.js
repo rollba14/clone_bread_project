36e3f9bf261c8c50721ec4674302f4516d5f7d67
Huan Ming Liao <hmliao14@gmail.com>
2019-04-08 22:16:23 -0700

    removed unused import in navbar.js from reactstrap

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,11 +7,7 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  NavLink} from 'reactstrap';
 
 
 class MyNavbar extends Component{
